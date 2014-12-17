ae2ca0f74eb9ce515e54190c5a2294a559e27d42
jack <jack.wgm@gmail.com>
2014-12-18 01:38:55 +0800

    更新vc项目文件为vc2013项目.
    Signed-off-by: jack <jack.wgm@gmail.com>

--- src/google/protobuf/compiler/ruby/ruby_generator.cc
+++ src/google/protobuf/compiler/ruby/ruby_generator.cc
@@ -29,6 +29,7 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <sstream>
+#include <cstdint>
 
 #include <google/protobuf/compiler/code_generator.h>
 #include <google/protobuf/compiler/plugin.h>
